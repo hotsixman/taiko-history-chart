2228492ea636f95af77399cf465ba65620abb2bd
hotsixisbest <galactagun12@gmail.com>
2024-02-23 23:48:51 +0900

    history-grid 추가

---
+++ src/components/historyGrid.ts
@@ -0,0 +1,32 @@
+import {LitElement, html, css} from 'lit';
+import {customElement} from 'lit/decorators.js';
+
+@customElement('history-grid')
+export class HistoryGrid extends LitElement{
+    static styles = css`
+        .history-container{
+            width:100%;
+            display:grid;
+
+            grid-template-columns: 100px auto;
+            grid-template-rows: auto 100px;
+        }
+    `
+
+    render(){
+        return html`
+            <div class="history-container">
+                <div>
+                    <slot name="date">
+                </div>
+                <div>
+                    <slot name="history">
+                </div>
+                <div></div>
+                <div>
+                    <slot name="gamecenter">
+                </div>
+            </div>
+        `
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import { HistoryGrid } from './components/historyGrid';
+
+export {HistoryGrid}
 
 (async() => {
     let data = (await axios({
